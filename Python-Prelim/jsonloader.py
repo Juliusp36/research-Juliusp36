5c1518f9b71e8bf382a0be8bc54285cb789a8830
Julius Parker <juliusparker776@gmail.com>
2025-10-07 21:05:35 -0400

    edited jsonloader

--- Python-Prelim/jsonloader.py
+++ Python-Prelim/jsonloader.py
@@ -1,8 +1,17 @@
 import pandas as pd
-import os
+import os #interact directly with the OS
 
+#obtain current directory and join it with AiM data file
 base_dir = os.path.dirname(__file__)
 file_path = os.path.join(base_dir, "JSON", "AiM-Phases-Last-3-Months.json")
 
-df = pd.read_json(file_path)
-print(df.head())
+dataframe = pd.read_json(file_path)
+print(dataframe.head())
+
+#flatten json file feilds
+#create and add to a database with all of the colums in the json file, with an 
+#additional column titled 'Summary' that begins null and will be filled by the AI generated summaries 
+# via API as requests are made
+
+#Summary column should pertain to the day and specific ticket right now
+#SQLite > os library > API
